3033025900602312f77c3186669b6e97f66c6dc9
Santosh <san0712kum@gmail.com>
2019-01-14 13:37:11 +0000

    New one

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using mysqltestproject.Models;
+
+namespace mysqltestproject.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            ProductStoreContext context = HttpContext.RequestServices.GetService(typeof(ProductStoreContext)) as ProductStoreContext;
+
+            return View(context.GetAllProducts());
+        }
+    }
+}
